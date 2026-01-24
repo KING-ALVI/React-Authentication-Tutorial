85dcd34fbaddcb698bcf37ff46c61ea264f3f839
KINGALVI <alvialmahamud@gmail.com>
2026-01-25 04:59:08 +0600

    edited some code

--- src/components/Main-Section/MainSection.jsx
+++ src/components/Main-Section/MainSection.jsx
@@ -1,17 +1,20 @@
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../Firebase-Authentication/FirebaseAuthentication";
 import { useState } from "react";
 
 const Main = () => {
 
-  // How to use Firebase Authentication system for Google sign in or log in .
+  // How to declare Firebase Authentication system for Google sign in or log in .
   const GoggleAuthentication = new GoogleAuthProvider();
 
   // How to show the user infomation using Firebase Authentication system .
-  const [GoogleUserInfo, setGoogleUserInfo] = useState(null);
+  const [GoogleUserInfo, setGoogleUserInfo] = useState([]);
+
 
   // How to use Firebase Authentication system for Google sign in or log in .
   const handelGoogleSignInAuthentication = () => {
+
+    // How to use Firebase Authentication system for Google sign in or log in .
     signInWithPopup(auth, GoggleAuthentication)
       .then(result => {
         console.log(result.user);
@@ -36,7 +39,11 @@ const Main = () => {
       })
   }
 
-  // const [EmailUserInfo, setEmailUserInfo] = useState(null);
+
+
+
+
+  const [EmailUserInfo, setEmailUserInfo] = useState([]);
 
   // How to show Sign in success massage .
   const [EmailSigninSuccessMassage, setEmailSigninSuccessMassage] = useState('');
@@ -47,13 +54,20 @@ const Main = () => {
   // How to show Password confirmation massage .
   const [PasswordConfirmationMassage, setPasswordConfirmationMassage] = useState('');
 
+
   // How to use Firebase Authentication system for Email sign in .
   const handelEmailSignInAuthentication = e => {
+
+    // Stop the Form from refreshing the website when the button is clicked .
     e.preventDefault();
+
+    // Get the Email value from the email input field .
     const Email = e.target.Email.value;
+
+    // Get the Password value from the password input field .
     const Password = e.target.Password.value;
 
-    // Clear the existing state when the form button is clicked .
+    // Clear the existing State when the form button is clicked .
     setEmailSigninSuccessMassage('');
     setEmailSigninExistsMassage('');
     setPasswordConfirmationMassage('');
@@ -70,21 +84,50 @@ const Main = () => {
     createUserWithEmailAndPassword(auth, Email, Password)
       .then(Result => {
         const UserData = Result.user;
-        // setEmailUserInfo(UserData);
+        setEmailUserInfo(UserData);
 
         // How to show Sign in success massage .
         setEmailSigninSuccessMassage(UserData);
 
-        console.log(Result);
+        console.log('Sign in data : ', UserData);
       })
       .catch(Error => {
         console.log(Error);
 
         // How to show Firebase error message .
-        // EmailSigninExistsMassage(Error.message);
+        setEmailSigninExistsMassage(Error.message);
       })
 
-    console.log('Email : ', Email, 'Password : ', Password)
+    console.log('Sign in Email : ', Email, 'Sign in Password : ', Password)
+  }
+
+
+
+
+
+  // How to use Firebase Authentication system for Email log in .
+  const handelEmailLogInAuthentication = e => {
+
+    // Stop the Form from refreshing the website when the button is clicked .
+    e.preventDefault();
+
+    // Get the Email value from the email input field .
+    const Email = e.target.Email.value;
+
+    // Get the Password value from the password input field .
+    const Password = e.target.Password.value;
+
+    // Firebase Email log in function .
+    signInWithEmailAndPassword(auth, Email, Password)
+      .then(Result => {
+        const UserData = Result.user
+        console.log('Log in data : ', UserData);
+      })
+      .catch(Error => {
+        console.log(Error)
+      })
+
+    console.log('Log in Email : ', Email, 'Log in Password : ', Password)
   }
 
 
@@ -123,7 +166,7 @@ const Main = () => {
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-              <form className="fieldset" onSubmit={null}>
+              <form className="fieldset" onSubmit={handelEmailSignInAuthentication}>
                 <label className="label">Email</label>
                 <input type="email" className="input" placeholder="Email" name="Email" />
                 <label className="label">Password</label>
@@ -143,7 +186,9 @@ const Main = () => {
          /* else */ : EmailSigninExistsMassage && <h1 className="text-2xl text-red-500">This email already exists !!</h1>
 
           }
-          {/* {EmailUserInfo.email} */}
+
+          {/* How to show the user infomation using Firebase Authentication system . */}
+          {EmailUserInfo.email}
         </div>
       </div>
 
@@ -155,7 +200,7 @@ const Main = () => {
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-              <form className="fieldset" onSubmit={handelEmailSignInAuthentication}>
+              <form className="fieldset" onSubmit={handelEmailLogInAuthentication}>
                 <label className="label">Email</label>
                 <input type="email" className="input" placeholder="Email" name="Email" />
                 <label className="label">Password</label>
